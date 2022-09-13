fa8955d01f5d7276d4164f053fe7164330f9802e
AlekseiZhmetko <a.zhmetko@gmail.com>
2022-09-13 17:03:57 +0300

    commit after HW review ver.2 + PEP8

--- task2.py
+++ task2.py
@@ -45,14 +45,62 @@ if __name__ == '__main__':
         for s in a.all():
             print(s.id, s.name)
 
-# чтобы выводились названия книг издательства:
+# вывод названий книг по издательству:
 
     pub = input('Введите id или название издателя, книги которого нужно вывести: ')
     if pub.isnumeric():
-        a = session.query(Publisher.id, Publisher.name, Book.title, Book.id).join(Book).filter(Publisher.id == f'{pub}')
+        a = session.query(Publisher.id, Publisher.name, Book.title, Book.id)\
+            .join(Book)\
+            .filter(Publisher.id == f'{pub}')
         for s in a.all():
             print(s.name, '|', s.title)
     else:
-        a = session.query(Publisher.id, Publisher.name, Book.title, Book.id).join(Book).filter(Publisher.name.like(f'%{pub}%'))
+        a = session.query(Publisher.id, Publisher.name, Book.title, Book.id)\
+            .join(Book)\
+            .filter(Publisher.name.like(f'%{pub}%'))
         for s in a.all():
             print(s.name, '|', s.title)
+
+# вывод магазинов (уникальных), где есть книги издательства:
+
+    pub = input('Введите id или название издателя, книги которого нужно вывести: ')
+    if pub.isnumeric():
+        a = session.query(Publisher.id, Publisher.name, Book.title, Book.id, Shop.name)\
+            .join(Book).join(Stock, Book.id == Stock.id_book)\
+            .join(Shop, Stock.id_shop == Shop.id)\
+            .filter(Publisher.id == f'{pub}')
+        shop_list = []
+        for s in a.all():
+            shop_list.append(s.name)
+        for s in set(shop_list):
+            print(s)
+    else:
+        a = session.query(Publisher.id, Publisher.name, Book.title, Book.id, Shop.name)\
+            .join(Book).join(Stock, Book.id == Stock.id_book)\
+            .join(Shop, Stock.id_shop == Shop.id)\
+            .filter(Publisher.name.like(f'%{pub}%'))
+        shop_list = []
+        for s in a.all():
+            shop_list.append(s.name)
+        for s in set(shop_list):
+            print(s)
+
+## Вывод магазина к каждой книге издательства:
+
+    pub = input('Введите id или название издателя, книги которого нужно вывести: ')
+    if pub.isnumeric():
+        a = session.query(Publisher.id, Publisher.name, Book.title, Book.id, Shop.name)\
+            .join(Book)\
+            .join(Stock, Book.id == Stock.id_book)\
+            .join(Shop, Stock.id_shop == Shop.id)\
+            .filter(Publisher.id == f'{pub}')
+        for s in a.all():
+            print(s.name)
+    else:
+        a = session.query(Publisher.id, Publisher.name, Book.title, Book.id, Shop.name)\
+            .join(Book)\
+            .join(Stock, Book.id == Stock.id_book)\
+            .join(Shop, Stock.id_shop == Shop.id)\
+            .filter(Publisher.name.like(f'%{pub}%'))
+        for s in a.all():
+            print(s.name)
