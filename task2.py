508dcec383fabe7f963a9141a6735f4afe7067dc
AlekseiZhmetko <a.zhmetko@gmail.com>
2022-09-13 16:33:18 +0300

    changes after HW review

--- task2.py
+++ task2.py
@@ -8,40 +8,51 @@ from task1 import Publisher, Shop, Book, Stock, Sale
 
 login = os.getenv('login')
 password = os.getenv('password')
-adress = os.getenv('adress')
+address = os.getenv('address')
 db_name = os.getenv('db_name')
 
-DSN = f'postgresql://{login}:{password}@{adress}/{db_name}'
+DSN = f'postgresql://{login}:{password}@{address}/{db_name}'
 engine = sqlalchemy.create_engine(DSN)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-with open('fixtures/tests_data.json', 'r') as fd:
-    data = json.load(fd)
+# with open('fixtures/tests_data.json', 'r') as fd:
+#     data = json.load(fd)
+#
+#     for record in data:
+#         model = {
+#             'publisher': Publisher,
+#             'shop': Shop,
+#             'book': Book,
+#             'stock': Stock,
+#             'sale': Sale,
+#         }[record.get('model')]
+#         session.add(model(id=record.get('pk'), **record.get('fields')))
+# session.commit()
 
-    for record in data:
-        model = {
-            'publisher': Publisher,
-            'shop': Shop,
-            'book': Book,
-            'stock': Stock,
-            'sale': Sale,
-        }[record.get('model')]
-        session.add(model(id=record.get('pk'), **record.get('fields')))
-session.commit()
+if __name__ == '__main__':
 
-pub = input('Введите id или название издателя: ')
-if pub.isnumeric():
-    a = session.query(Publisher).filter(Publisher.id == f'{pub}')
-    for s in a.all():
-        print(s.id, s.name)
-else:
-    a = session.query(Publisher).filter(Publisher.name.like(f'%{pub}%'))
-    for s in a.all():
-        print(s.id, s.name)
+# чтобы выводилось название издательства и его id:
 
-# a = session.query(Publisher).filter(or_(Publisher.id == f'{pub_id}', Publisher.name == f'{pub}'))
-# for s in a.all():
-#     print(s.id, s.name)
-## в таком виде ввод в виде строки не проходит проверку на условие того, что id - int
+    pub = input('Введите id или название издателя: ')
+    if pub.isnumeric():
+        a = session.query(Publisher).filter(Publisher.id == f'{pub}')
+        for s in a.all():
+            print(s.id, s.name)
+    else:
+        a = session.query(Publisher).filter(Publisher.name.like(f'%{pub}%'))
+        for s in a.all():
+            print(s.id, s.name)
+
+# чтобы выводились названия книг издательства:
+
+    pub = input('Введите id или название издателя, книги которого нужно вывести: ')
+    if pub.isnumeric():
+        a = session.query(Publisher.id, Publisher.name, Book.title, Book.id).join(Book).filter(Publisher.id == f'{pub}')
+        for s in a.all():
+            print(s.name, '|', s.title)
+    else:
+        a = session.query(Publisher.id, Publisher.name, Book.title, Book.id).join(Book).filter(Publisher.name.like(f'%{pub}%'))
+        for s in a.all():
+            print(s.name, '|', s.title)
